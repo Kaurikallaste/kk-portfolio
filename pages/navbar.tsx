93992e53550e3518acfb0439d49d53df4c12b048
KAURI KALLASTE <kaurikallaste@gmail.com>
2022-02-24 14:10:17 +0200

    lisasin hetkese asukoha linki highlightimise

--- pages/navbar.tsx
+++ pages/navbar.tsx
@@ -1,12 +1,17 @@
 import type { NextComponentType } from 'next';
 import Link from 'next/link';
+import {useRouter} from 'next/router';
+
 
 const Navbar: NextComponentType = () => {
+    const router = useRouter();
+    console.log(router.pathname);
+
     return (
         <ul className="navbar">
 
-            <Link href="/"><a><li> PROJEKTID </li></a></Link>
-            <Link href="kontakt"><a><li> KONTAKT </li></a></Link>
+            <Link href="/"><a><li className={router.pathname == "/" ? "current-url" : ""}> PROJEKTID </li></a></Link>
+            <Link href="/kontakt"><a><li className={router.pathname == "/kontakt" ? "current-url" : ""}> KONTAKT </li></a></Link>
             <Link href="https://github.com/Kaurikallaste" prefetch={false}><a target="_blank"><li> GITHUB </li></a></Link>
         </ul>
     );
